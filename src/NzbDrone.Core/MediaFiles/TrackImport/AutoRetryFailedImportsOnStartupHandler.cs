d34d9352ca19eb62f9d0f695fc57fadb749cc12c
Jack Danger <github@jackdanger.com>
2026-05-07 16:47:49 -0700

    Trigger immediate root folder scan on startup for failed imports retry

--- src/NzbDrone.Core/MediaFiles/TrackImport/AutoRetryFailedImportsOnStartupHandler.cs
+++ src/NzbDrone.Core/MediaFiles/TrackImport/AutoRetryFailedImportsOnStartupHandler.cs
@@ -1,5 +1,7 @@
 using NLog;
 using NzbDrone.Core.Lifecycle;
+using NzbDrone.Core.MediaFiles.Commands;
+using NzbDrone.Core.Messaging.Commands;
 using NzbDrone.Core.Messaging.Events;
 
 namespace NzbDrone.Core.MediaFiles.TrackImport
@@ -7,10 +9,12 @@ namespace NzbDrone.Core.MediaFiles.TrackImport
     public class AutoRetryFailedImportsOnStartupHandler : IHandle<ApplicationStartedEvent>
     {
         private readonly Logger _logger;
+        private readonly IManageCommandQueue _commandQueueManager;
 
-        public AutoRetryFailedImportsOnStartupHandler(Logger logger)
+        public AutoRetryFailedImportsOnStartupHandler(Logger logger, IManageCommandQueue commandQueueManager)
         {
             _logger = logger;
+            _commandQueueManager = commandQueueManager;
         }
 
         public void Handle(ApplicationStartedEvent message)
@@ -18,7 +22,9 @@ namespace NzbDrone.Core.MediaFiles.TrackImport
             _logger.Info(
                 "Application started - improved import matching active. Unimported files will be automatically " +
                 "re-evaluated with pre-normalization, fuzzy matching, multi-disc detection, and other improvements. " +
-                "Next automatic root folder scan will process these with new matching logic.");
+                "Triggering immediate root folder scan to process pending imports.");
+
+            _commandQueueManager.Push(new RescanFoldersCommand(), CommandTrigger.Manual);
         }
     }
 }
